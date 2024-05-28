c6fda7d9ed2cf27ac4a9e4e0d993a472a278684d
Ashwani <ashwaniaryatech@gmail.com>
2024-05-28 21:32:17 +0530

    GFG- LargestElementInArray

---
+++ geeksForGeeks/LargestElementInArray.java
@@ -0,0 +1,46 @@
+package geeksForGeeks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LargestElementInArray {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        int t =
+                Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Compute obj = new Compute();
+            System.out.println(obj.largest(a, n));
+
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Compute {
+
+    public int largest(int arr[], int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
